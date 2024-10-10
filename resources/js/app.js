d07c3c498a5fe25c36b29ad41696ecd4355a1cfe
KeyderDev <angelmolina02111@gmail.com>
2024-10-10 00:25:44 +0000

    changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
